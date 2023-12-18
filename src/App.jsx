815b21ab23cf9b08b6398ddf1ef0c89b874330f9
tigerweirdo <temmuzcetiner@gmail.com>
2023-12-18 17:48:47 +0300

    AdminPage-Scrolltotop

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,21 @@ import CartPage from "./pages/CartPage";
 import AuthPage from "./pages/AuthPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import BlogDetailsPage from "./pages/BlogDetailsPage";
-import UserPage from "./pages/admin/UserPage";
-import CategoryPage from "./pages/admin/Categories/CategoryPage";
-import UpdateCategoryPage from "./pages/admin/Categories/UpdateCategoryPage";
-import CreateCategoryPage from "./pages/admin/Categories/CreateCategoryPage";
-import CreateProductPage from "./pages/admin/Products/CreateProductPage";
-import ProductPage from "./pages/admin/Products/ProductPage";
-import UpdateProductPage from "./pages/admin/Products/UpdateProductPage";
-import CouponPage from "./pages/admin/Coupons/CouponPage";
-import CreateCouponPage from "./pages/admin/Coupons/CreateCouponPage";
-import UpdateCouponPage from "./pages/admin/Coupons/UpdateCouponPage";
+
 import "./App.css";
+import UserPage from "./pages/Admin/UserPage";
+import CategoryPage from "./pages/Admin/Categories/CategoryPage";
+import UpdateCategoryPage from "./pages/Admin/Categories/UpdateCategoryPage";
+import CreateCategoryPage from "./pages/Admin/Categories/CreateCategoryPage";
+import CreateProductPage from "./pages/Admin/Products/CreateProductPage";
+import ProductPage from "./pages/Admin/Products/ProductPage";
+import UpdateProductPage from "./pages/Admin/Products/UpdateProductPage";
+import CouponPage from "./pages/Admin/Coupons/CouponPage";
+import CreateCouponPage from "./pages/Admin/Coupons/CreateCouponPage";
+import UpdateCouponPage from "./pages/Admin/Coupons/UpdateCouponPage";
+import Success from "./pages/Success";
+import OrderPage from "./pages/Admin/OrderPage";
+import DashboardPage from "./pages/Admin/DashboardPage";
 
 function App() {
   return (
@@ -30,17 +34,20 @@ function App() {
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/product/:id" element={<ProductDetailsPage />} />
       <Route path="/blog/:id" element={<BlogDetailsPage />} />
+      <Route path="/success" element={<Success />} />
       <Route path="/admin/*">
-      <Route path="users" element={<UserPage />} />
+        <Route index element={<DashboardPage />} />
+        <Route path="users" element={<UserPage />} />
         <Route path="categories" element={<CategoryPage />} />
-        <Route path="categories/update/:id" element={<UpdateCategoryPage />} />
         <Route path="categories/create" element={<CreateCategoryPage />} />
+        <Route path="categories/update/:id" element={<UpdateCategoryPage />} />
         <Route path="products" element={<ProductPage />} />
         <Route path="products/create" element={<CreateProductPage />} />
         <Route path="products/update/:id" element={<UpdateProductPage />} />
         <Route path="coupons" element={<CouponPage />} />
         <Route path="coupons/create" element={<CreateCouponPage />} />
         <Route path="coupons/update/:id" element={<UpdateCouponPage />} />
+        <Route path="orders" element={<OrderPage />} />
       </Route>
     </Routes>
   );
